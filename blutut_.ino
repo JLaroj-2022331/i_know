e6d851996429c7cb78f7c5a31aa7930d25e14a5c
JLaroj-2022331 <128552124+JLaroj-2022331@users.noreply.github.com>
2023-07-13 17:14:26 -0600

    Update blutut_.ino

--- blutut_.ino
+++ blutut_.ino
@@ -1,28 +1,103 @@
-#include <SoftwareSerial.h>
-SoftwareSerial ok(10, 11);
 
-void setup() 
+#include <LedControl.h>
+#include <Ticker.h>
+
+int valorS, ok;
+int seg;
+
+volatile static bool estado_mq2;
+void isr_sensor(void); 
+
+LedControl lc=LedControl(11,13,10,1);
+Ticker isr_accion_SensorG(isr_sensor, 2000);
+
+void setup()
 {
 Serial.begin(9600);
-ok.begin(38400);
-pinMode(12, OUTPUT);
+lc.shutdown(0,false);
+lc.setIntensity(0,4);
+lc.clearDisplay(0);
+isr_accion_SensorG.start(); 
+}
+void loop()
+{
+isr_accion_SensorG.update();
 }
 
-char recibir;
-void loop() 
+void copo_God()
 {
-if(ok.available())
+byte copo_nieve[8]=
 {
-recibir = ok.read();
-Serial.println(recibir);
+  B00111100,       
+  B10011011,
+  B01011010,
+  B00111100,
+  B11100111,
+  B00111100,
+  B01011010,
+  B10011001
+};
 
-if(recibir == '1')
+for(int i=0; i<8;i++)
 {
-  digitalWrite(12, HIGH);
-}
-if(recibir == '2')
+lc.setRow(0,i,copo_nieve[i]);
+}}
+void llama_good()
 {
-  digitalWrite(12, LOW);
+
+byte llama[8]=
+{
+  B00000101,        
+  B00000010,
+  B00011100,
+  B00101010,
+  B01000011,
+  B11000011,
+  B01100010,
+  B00111100
+};  
+
+for(int i=0; i<8;i++)
+{
+lc.setRow(0,i,llama[i]);
+}}
+void isr_sensor(void)
+{
+valorS=analogRead(A5);
+ok = map(valorS,0,1024,0,100);
+Serial.print("adc:");
+Serial.print(valorS);
+Serial.println("   porcentaje:");
+Serial.println(ok);
+Serial.println(valorS);
+if(valorS>400)
+{
+if(seg==0)
+{
+parpadeo();}
+if(seg==1)
+{
+llama_good();
 }
 }
+
+else{
+copo_God();
+seg=0;
+}
+}
+
+void parpadeo()
+{
+llama_good();
+delay(1000);  
+lc.clearDisplay(0);
+delay(1000);
+llama_good();
+delay(1000);  
+lc.clearDisplay(0);
+delay(1000);
+llama_good();
+delay(1000);  
+seg=1;
 }
